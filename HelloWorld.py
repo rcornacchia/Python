9009cfebe17d2c1d35f6d84bc385dc24956a3fe1
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-09-21 15:41:21 -0400

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+print 'Hello World!'
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+x = "bobo"
+
+def printTwice(str):
+    print str
+    print str
